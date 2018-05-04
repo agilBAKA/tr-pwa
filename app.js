db70c5f718e53988dbfa06a03f9115415fd73758
Macintosh <agil@tlab.co.id>
2018-05-04 13:36:36 +0700

    pwa jogjajs

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-//This is the "Offline page" service worker
-
-//Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
 if (navigator.serviceWorker.controller) {
-  console.log('[PWA Builder] active service worker found, no need to register')
+  console.log('SW registered!')
 } else {
-  //Register the ServiceWorker
   navigator.serviceWorker.register('service-worker.js', {
     scope: './'
   }).then(function(reg) {
